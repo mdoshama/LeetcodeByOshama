9a2a49fca3998e55e22a39e49012402aa68a9634
oshama <info.oshama@gmail.com>
2023-10-28 00:23:38 +0530

    Problem 1

--- One.java
+++ One.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class One {
+import java.util.HashMap;
+import java.util.Map;
+
+public class One {
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        int target = 9;
+
+        int[] val = twoSum(nums, target);
+        for(int value : val) {
+            System.out.println(value);
+        }
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numToIndex = new HashMap<>();
+        for(int i = 0; i < nums.length; ++i) {
+            if(numToIndex.containsKey(target - nums[i])) {
+                return new int[] {numToIndex.get(target - nums[i]), i};
+            }
+            numToIndex.put(nums[i], i);
+        }
+        throw new IllegalArgumentException();
+
+    }
 }
